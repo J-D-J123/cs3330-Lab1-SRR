d9445cb0203ce5fa74c838990860da243dfc4387
J-eyJ <60826745+J-D-J123@users.noreply.github.com>
2026-02-17 13:49:58 -0600

    ReservationBook outline

---
+++ GroupAssignment1_SRR/src/edu/mizzou/cs3330/group1/srr/ReservationBook.java
@@ -0,0 +1,17 @@
+package edu.mizzou.cs3330.group1.srr;
+
+public class ReservationBook {
+
+	private Reservation[] 	reservations; 
+	private int 			count; 
+	
+	// Constructor creates an empty book with a fixed maximum capacity > 0 
+	
+	// add(Reservation r) adds if space exists otherwise throws an exception 
+	
+	// findById(int id) returns the matching reservation or null if not found 
+	
+	// printAll() prints all stored reservations (one per line) -> system.out.print
+	
+	// printForRoom(Room room) prints reservations for that room 
+}
