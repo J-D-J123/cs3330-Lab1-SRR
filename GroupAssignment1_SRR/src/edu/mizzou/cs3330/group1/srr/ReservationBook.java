8a7bb4a2049a26a92be21c07c1f0a04e3bb6e745
J-eyJ <60826745+J-D-J123@users.noreply.github.com>
2026-02-18 13:48:51 -0600

    ReservationBook.java updated

--- GroupAssignment1_SRR/src/edu/mizzou/cs3330/group1/srr/ReservationBook.java
+++ GroupAssignment1_SRR/src/edu/mizzou/cs3330/group1/srr/ReservationBook.java
@@ -1,19 +1,50 @@
 package edu.mizzou.cs3330.group1.srr;
 
+
+/**
+ * File:		ReservationBook.java
+ * Date:		02/18/2026
+ * Desc:		This class stores reservations and OWNS them. 
+ * 				It should NOT coordinate the whole application, it 
+ * 				should manage reservation storage and basic queries.
+ */
+
 public class ReservationBook {
 
 	private Reservation[] 	reservations; 
-	private int 			count; 
+	private int 				count; 
 	
 	// Constructor creates an empty book with a fixed maximum capacity > 0 
-	public ReservationBook() {
+	public ReservationBook(int maximumCapacity) {
 		
-		// call the Reservation class to set up the maximum capacity > 0
+		// if maxCapacity is <= 0 do not make 
+		if(maximumCapacity <= 0) {
+			
+			// print err in system 
+			System.err.println("Unable to create Reservation Book with size" + 
+					maximumCapacity);
+			
+			return; 
+		}
+		
+		// make the empty book aka empty array of reservations 
 	}
 	
 	// add(Reservation r) adds if space exists otherwise throws an exception 
+	public void add(Reservation r) {
+		
+		// add Reservation r to the reservations array
+		// if not throw an error 
+	}
 	
 	// findById(int id) returns the matching reservation or null if not found 
+	public Reservation findById(int id) {
+		
+		// just searches for Reservation r with r.id
+		
+		
+		return null; 
+	}
 	
 	// printAll() prints all stored reservations (one per line) -> system.out.print
 	
