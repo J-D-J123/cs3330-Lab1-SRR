a0d390065903096df286d7a53c9fa89bf581a4ed
J-eyJ <60826745+J-D-J123@users.noreply.github.com>
2026-02-19 09:01:24 -0600

    Room updated with javadoc comments

--- GroupAssignment1_SRR/src/edu/mizzou/cs3330/group1/srr/Room.java
+++ GroupAssignment1_SRR/src/edu/mizzou/cs3330/group1/srr/Room.java
@@ -5,7 +5,11 @@ public class Room {
 	private String roomName;
 	private int capacity;
 	
-	// Constructs a Room with a name and positive capacity, throws exception if invalid
+	/**
+	 * Room() constructs a Room with a name and positive capacity, throws exception if invalid
+	 * @param roomNum a String to represent the number of the room
+	 * @param maxPeople an integer that represents the maximum amount of people in that Room
+	 */
 	public Room(String roomNum, int maxPeople) {
 		if(maxPeople <= 0) {
 			throw new IllegalArgumentException("Needs to be at least one person");
@@ -19,17 +23,25 @@ public class Room {
 		this.roomName = roomNum;
 	}
 
-	// Returns the name of the room
+	/**
+	 * getRoomName() returns the name of the room
+	 * @return a String aka roomName; 
+	 */
 	public String getRoomName() {
 		return roomName;
 	}
 
-	// Returns the capacity of the room
+	/**
+	 * getCapacity() returns the capacity of the room
+	 * @return an integer 
+	 */
 	public int getCapacity() {
 		return capacity;
 	}
 	
-	// Returns a readable description of the room, e.g. "Room 101 (cap 4)"
+	/**
+	 * toString() returns a readable description of the room, e.g. "Room 101 (cap 4)
+	 */
 	@Override
 	public String toString() {
 		return (roomName + " (cap " + capacity + ")");
--- GroupAssignment1_SRR/src/edu/mizzou/cs3330/group1/srr/TimeSlot.java
+++ GroupAssignment1_SRR/src/edu/mizzou/cs3330/group1/srr/TimeSlot.java
@@ -4,7 +4,10 @@ public class TimeSlot {
 	
 	private int hour;
 	
-	// Constructs a TimeSlot with an hour between 0 and 23, throws exception if invalid
+	/**
+	 * Constructs a TimeSlot with an hour between 0 and 23, throws exception if invalid
+	 * @param time an int between 0 and 23 
+	 */
 	public TimeSlot(int time) {
 		if((time < 0) || (time > 23)) {
 			throw new IllegalArgumentException("Not acceptable time range");
@@ -12,12 +15,18 @@ public class TimeSlot {
 		hour = time;
 	}
 
-	// Returns the hour of this time slot
+	/**
+	 * getHour() returns the hour of this time slot
+	 * @return the integer hour
+	 */
 	public int getHour() {
 		return hour;
 	}
 	
-	// Returns a string showing the one-hour time slot, e.g. "13:00-14:00"
+
+	/**
+	 * toString() returns a String showing the one-hour time slot, e.g. "13:00-14:00"
+	 */
 	public String toString() {
 		return (hour + ":00"+ "-" + (hour + 1) + ":00");
 	}
