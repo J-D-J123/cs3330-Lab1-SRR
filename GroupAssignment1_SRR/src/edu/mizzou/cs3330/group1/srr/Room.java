5a2b9e9c9fe53aee94aa9609b7c19c51bdf93c8b
Tucker <151984953+TuckerPMIZ@users.noreply.github.com>
2026-02-18 19:17:34 -0600

    Finished the room object

--- GroupAssignment1_SRR/src/edu/mizzou/cs3330/group1/srr/Room.java
+++ GroupAssignment1_SRR/src/edu/mizzou/cs3330/group1/srr/Room.java
@@ -1,5 +1,32 @@
 package edu.mizzou.cs3330.group1.srr;
 
 public class Room {
+  private String roomName;
+	private int capacity;
+	
+	public Room(String roomNum, int maxPeople) {
+		if(maxPeople <= 0) {
+			throw new IllegalArgumentException("Needs to be at least one person");
+		}
+		this.capacity = maxPeople;
+		
+		if(roomNum == null || roomNum.isBlank()) {
+			throw new IllegalArgumentException("Needs to be a room number");
+		}
+		this.roomName = roomNum;
+	}
 
+	public String getRoomName() {
+		return roomName;
+	}
+
+	public int getCapacity() {
+		return capacity;
+	}
+	
+	@Override
+	public String toString() {
+		return (roomName + " (cap " + capacity + ")");
+	}
 }
+
