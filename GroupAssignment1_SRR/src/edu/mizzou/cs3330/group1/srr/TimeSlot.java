ccbafea0d2db1946643f515d6607954cd4b386e0
Tucker <151984953+TuckerPMIZ@users.noreply.github.com>
2026-02-18 19:18:20 -0600

    TimeSlot finished

--- GroupAssignment1_SRR/src/edu/mizzou/cs3330/group1/srr/TimeSlot.java
+++ GroupAssignment1_SRR/src/edu/mizzou/cs3330/group1/srr/TimeSlot.java
@@ -1,5 +1,21 @@
 package edu.mizzou.cs3330.group1.srr;
 
 public class TimeSlot {
+  private int hour;
+	
+	public TimeSlot(int time) {
+		if(time < 0 || time > 23) {
+			throw new IllegalArgumentException("Not acceptable time range");
+		}
+		hour = time;
+	}
 
+	public int getHour() {
+		return hour;
+	}
+	
+	public String toString() {
+		return (hour + ":00"+ "-" + (hour + 1) + ":00");
+	}
 }
+
